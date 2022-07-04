e60b4218c7ded530a6fcfc37f258aa930ae5906f
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-07-04 22:05:13 +0530

    Few more program

---
+++ HashMap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HashMap{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Map<String,String> ctryCapitalMap = new HashMap<>();
+        System.out.println("\nEnter the count:");
+        int number = sc.nextInt();
+        for(int i=0;i<number;i++){
+            String fn = sc.nextLine();
+            String ln = sc.nextLine();
+            names.add(fn,ln);
+        }
+    }
+}
