de98e3f6d3051a0e96e15e525a22008fda163013
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-07-21 19:06:07 +0530

    Pattern printing

--- Patternprint1.java
+++ Patternprint1.java
@@ -5,7 +5,10 @@ public class Patternprint1 {
         // pattern2(5);
         // pattern3(5);
         // pattern4(5);
-        pattern5(5);
+        // pattern5(5);
+        // pattern28(5);
+        // pattern30(5);
+        pattern17(4);
     }
     static void pattern1(int n){
         System.out.println("\nPattern 1:\n");
@@ -57,4 +60,57 @@ public class Patternprint1 {
             System.out.println();
         }
     }
+    static void pattern28(int n){
+        for(int row=0;row<2*n;row++){
+            int totalCol = row> n ? 2 * n - row : row;
+            int spaces = n-totalCol;
+            for(int i=0 ;i<spaces;i++)
+            {
+                System.out.print(" ");
+            }
+            for(int col=0;col<totalCol;col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern30(int n){
+        for(int row=1;row<=n;row++)
+        {
+            int spaces = n-row;
+            for(int i=0;i<=spaces;i++)
+            {
+                System.out.print("  ");
+            }
+            for(int col=row;col>=1;col--){ 
+                System.out.print(col+" ");
+            }
+            for(int col2=2;col2<=row;col2++)
+            {
+                System.out.print(col2+" ");
+            }
+            System.out.println();
+        }
+}
+
+    static void pattern17(int n){
+        for(int row=1;row<= 2*n ;row++)
+        {
+            int c = row>n? 2*n-row:row;
+            for(int space=0;space<n-c;space++)
+            {
+                System.out.print("  ");
+            }
+            for(int col=c;col>=1;col--){ 
+                System.out.print(col+" ");
+            }
+            for(int col=2;col<=c;col++)
+            {
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+}
 }
